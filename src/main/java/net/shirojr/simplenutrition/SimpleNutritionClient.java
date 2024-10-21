701cfd291f38a921296088e2ed001a611e087381
JR1811 <janreichelt77@gmail.com>
2024-10-22 00:00:57 +0200

    added client entrypoint

---
+++ src/main/java/net/shirojr/simplenutrition/SimpleNutritionClient.java
@@ -0,0 +1,10 @@
+package net.shirojr.simplenutrition;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleNutritionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
