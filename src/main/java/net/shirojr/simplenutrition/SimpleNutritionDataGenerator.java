58eb784c17f8db1545db91aa1ddfe0dc8dc9f11d
JR1811 <janreichelt77@gmail.com>
2024-10-21 23:57:31 +0200

    Initial commit

---
+++ src/main/java/net/shirojr/simplenutrition/SimpleNutritionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shirojr.simplenutrition;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleNutritionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
